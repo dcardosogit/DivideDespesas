53594ccfc76b3fd241bbee5ab4a4c3e5bcc9fcb1
Diego <dcardoso86@gmail.com>
2017-08-23 22:00:18 -0300

    Adição dos 10%

--- app/src/main/java/br/com/opet/tds/dividedespesas/MainActivity.java
+++ app/src/main/java/br/com/opet/tds/dividedespesas/MainActivity.java
@@ -2,9 +2,13 @@ package br.com.opet.tds.dividedespesas;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +16,8 @@ public class MainActivity extends Activity {
     private SeekBar seekQtdePessoas;
     private TextView textQtdePessoas;
     private TextView textDivisaoDespesa;
+    private CheckBox checkBoxAdicional;
+    private double adicional;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +28,27 @@ public class MainActivity extends Activity {
         seekQtdePessoas = (SeekBar) findViewById(R.id.seekQtdePessoas);
         textQtdePessoas = (TextView) findViewById(R.id.textTotalPessoas);
         textDivisaoDespesa = (TextView) findViewById(R.id.textDivisaoDespesa);
+        checkBoxAdicional = (CheckBox) findViewById(R.id.checkboxAdicional);
+
+        adicional = 1.0;
+
+        checkBoxAdicional.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                adicional = isChecked ? 1.1 :  1.0;
+                double totalDespesa = Double.parseDouble(editDespesa.getText().toString()) * adicional;
+                double despesa = totalDespesa / seekQtdePessoas.getProgress();
+                textDivisaoDespesa.setText(String.format("%.2f", despesa));
+            }
+        });
 
         seekQtdePessoas.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 if(!editDespesa.getText().toString().isEmpty() && progress != 0){
-                    double totalDespesa = Double.parseDouble(editDespesa.getText().toString());
+                    double totalDespesa = Double.parseDouble(editDespesa.getText().toString()) * adicional;
                     double despesa = totalDespesa / progress;
-                    textDivisaoDespesa.setText(String.valueOf(despesa));
+                    textDivisaoDespesa.setText(String.format("%.2f", despesa));
                     textQtdePessoas.setText(String.valueOf(progress));
                 }
             }
